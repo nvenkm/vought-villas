ccb8d7350c8ed7f3dff9f783478b2e3cf37ba28c
nvenkm <nkbossgolden@gmail.com>
2023-10-10 23:48:23 +0530

    database connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import env from "dotenv";
+
+env.config();
+
+const uri = process.env.URI;
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((e) => {
+    console.log(e.message);
+  });
 
 const app = express();
 
